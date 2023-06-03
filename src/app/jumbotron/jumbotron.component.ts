589434a0254598dc1ef1361f60a4ca5e3f954533
ImsirovicAjdin <ajdika@live.com>
2023-06-03 14:44:19 +0200

    Make Jumbotron component serve content conditionally - based on in which component it's used

--- src/app/jumbotron/jumbotron.component.ts
+++ src/app/jumbotron/jumbotron.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-jumbotron',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./jumbotron.component.scss']
 })
 export class JumbotronComponent {
-
+  @Input() title: string = '';
 }
