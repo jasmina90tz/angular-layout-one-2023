a5ef8a26dfa24a33138fa7ff6ca3d3de40294ee9
ImsirovicAjdin <ajdika@live.com>
2023-06-03 13:10:52 +0200

    Update nav so that it's like the source layout

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -6,8 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-  constructori() { }
+  isMobileMenuOpen = false;
+
+  constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
   }
 }
