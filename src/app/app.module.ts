fb142b67525ca13d9d98094bd4a12fd6f9b7b04d
jasmina.zukancic <jasmina.zukancic@fsbtech.com>
2023-06-11 17:32:35 +0200

    home screen responsive and working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,7 +36,7 @@ import { ServicesComponent } from './services/services.component';
     TextOverImageComponent,
     ServicesComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
