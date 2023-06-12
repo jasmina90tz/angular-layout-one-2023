f3fd13f7fc18c80753c5529154c924ece8ada445
jasmina.zukancic <jasmina.zukancic@fsbtech.com>
2023-06-12 09:45:09 +0200

    upload added

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -4,7 +4,7 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
   formData: any = {};
