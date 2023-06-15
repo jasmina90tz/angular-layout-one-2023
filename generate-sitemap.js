4a5a203178b916f6346aa4f35e2372940e46f292
jasmina.zukancic <jasmina.zukancic@fsbtech.com>
2023-06-15 13:42:21 +0200

    xml added for sitemap

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,15 +1,42 @@
-const SitemapGenerator = require("sitemap-generator");
+const fs = require("fs-extra");
 const path = require("path");
-const targetUrl = "https://alphamedical-bih.com";
-const sitemapPath = path.resolve(__dirname, "dist", "sitemap.xml");
-const generator = SitemapGenerator(targetUrl, {
-  filepath: sitemapPath,
-  stripQuerystring: true,
-});
-generator.start();
-generator.on("error", (error) => {
-  console.error("Error generating sitemap:", error);
-});
-generator.on("done", () => {
-  console.log("Sitemap generation completed!");
-});
+const { SitemapStream, streamToPromise } = require("sitemap");
+
+// Define the URLs for your website
+const urls = [
+  { url: "/", changefreq: "weekly", priority: 1.0 },
+  { url: "/about-us", changefreq: "monthly", priority: 0.8 },
+  { url: "/contact", changefreq: "monthly", priority: 0.8 },
+  { url: "/for-applicants", changefreq: "monthly", priority: 0.8 },
+  { url: "/for-companies", changefreq: "monthly", priority: 0.8 },
+  { url: "/home", changefreq: "monthly", priority: 0.8 },
+  // Add more URLs as needed
+];
+
+// Function to generate the XML sitemap
+async function generateSitemap() {
+  const sitemap = new SitemapStream({
+    hostname: "https://alphamedical-bih.com",
+  });
+
+  // Add the URLs to the sitemap
+  urls.forEach((url) => {
+    sitemap.write(url);
+  });
+
+  sitemap.end();
+
+  // Convert the sitemap stream to a string
+  const sitemapXml = await streamToPromise(sitemap).toString();
+
+  // Set the output path and filename for the sitemap
+  const outputPath = path.join(__dirname, "dist", "sitemap.xml");
+
+  // Write the sitemap XML to the output file
+  fs.outputFileSync(outputPath, sitemapXml);
+
+  console.log("Sitemap generated successfully!");
+}
+
+// Invoke the function to generate the sitemap
+generateSitemap();
