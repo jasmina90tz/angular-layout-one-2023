677f91b61eb82e3ef434826a80fd1d15e7d5bee1
jasmina.zukancic <jasmina.zukancic@fsbtech.com>
2023-06-26 10:07:54 +0200

    changed jumbotron

--- src/app/jumbotron/jumbotron.component.ts
+++ src/app/jumbotron/jumbotron.component.ts
@@ -1,10 +1,17 @@
 import { Component, Input } from '@angular/core';
+import { SafeResourceUrl } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-jumbotron',
   templateUrl: './jumbotron.component.html',
-  styleUrls: ['./jumbotron.component.scss']
+  styleUrls: ['./jumbotron.component.scss'],
 })
 export class JumbotronComponent {
   @Input() title: string = '';
+  gifUrl: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer) {
+    const path = 'assets/images/gif.gif';
+    this.gifUrl = this.sanitizer.bypassSecurityTrustResourceUrl(path);
+  }
 }
