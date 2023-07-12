6b76650dddb23b8f7b481d7148e272ce9afa63e3
jasmina.zukancic <jasmina.zukancic@fsbtech.com>
2023-07-12 16:04:08 +0200

    kontakt forma vracena

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -8,29 +7,5 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-  formData = {
-    name: '',
-    lastname: '',
-    phone: '',
-    email: '',
-    message: '',
-    country: '',
-  };
-  constructor(private http: HttpClient) {}
-
-  submitForm() {
-    // Make an HTTP POST request to your Netlify function endpoint
-    this.http
-      .post('/.netlify/functions/your-function-name', this.formData)
-      .subscribe(
-        (response) => {
-          // Handle the success response
-          console.log('Form submission successful', response);
-        },
-        (error) => {
-          // Handle the error response
-          console.error('Form submission failed', error);
-        }
-      );
-  }
+  formData = {};
 }
