0d74a1b6fe054c4ffa19386ac86682ffc641a7e9
jasmina.zukancic <jasmina.zukancic@fsbtech.com>
2023-07-12 09:34:41 +0200

    updates

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -7,14 +8,29 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-  formData: any = {};
+  formData = {
+    name: '',
+    lastname: '',
+    phone: '',
+    email: '',
+    message: '',
+    country: '',
+  };
+  constructor(private http: HttpClient) {}
 
-  submitForm(form: NgForm) {
-    if (form.valid) {
-      // Perform any desired action with the form data
-      console.log(this.formData);
-      // Clear the form
-      form.reset();
-    }
+  submitForm() {
+    // Make an HTTP POST request to your Netlify function endpoint
+    this.http
+      .post('/.netlify/functions/your-function-name', this.formData)
+      .subscribe(
+        (response) => {
+          // Handle the success response
+          console.log('Form submission successful', response);
+        },
+        (error) => {
+          // Handle the error response
+          console.error('Form submission failed', error);
+        }
+      );
   }
 }
